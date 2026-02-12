b3b8de585857576d6a72ad77b2e7636b2d8adb56
Shubhaan Kumar Mishra <mishrashubhaankumar@gmail.com>
2026-02-12 11:08:43 +0530

    Added Assignment 4

---
+++ Assignmant 4/ques2.js
@@ -0,0 +1,36 @@
+
+const fs = require('fs');
+function wordCap(){
+    fs.readFile("a.txt","UTF-8",(err,data)=>{
+        if(err) throw err
+            let res = data.toUpperCase();
+            fs.writeFileSync("a.txt",`\n${res}`);
+    })
+}
+
+function revStr(){
+    fs.readFile("a.txt","UTF-8",(err,data)=>{
+        if(err) throw err
+            let res = data;
+            let rev = "";
+            for(let i = res.length-1; i>=0;i--){
+                rev = rev + res[i];
+            }
+            fs.writeFileSync("a.txt",`${rev}`);
+    })
+}
+
+function countVowels(){
+    fs.readFile("a.txt","UTF-8",(err,data)=>{
+        if(err) throw err
+        const vowels = ['a','e','i','o','u'];
+        let count = 0;
+        for(let character of data.toLowerCase()){
+            if(vowels.includes(character)){
+                count++;
+            }
+        }
+        fs.writeFileSync("countedVowels.txt",` total vowels count: ${count}`);
+    })
+}
+module.exports={wordCap,revStr,countVowels};
